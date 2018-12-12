1ce9681c99797ba861f570b523b9f4a09ba5f89a
Banan <petr.banan.hajek@gmail.com>
2018-12-13 00:56:08 +0100

    Added pagination to search users

--- modules/users/server/controllers/users.profile.server.controller.js
+++ modules/users/server/controllers/users.profile.server.controller.js
@@ -1489,6 +1489,8 @@ exports.search = function (req, res, next) {
     });
   }
 
+
+
   // perform the search
   User
     .find({ $and: [
@@ -1503,7 +1505,9 @@ exports.search = function (req, res, next) {
     .select(exports.userMiniProfileFields + ' -_id')
     .sort({ score: { $meta: 'textScore' } })
     // limit the amount of found users (config)
-    .limit(config.limits.userSearchLimit)
+    .limit(config.limits.userSearchPageLimit)
+    // skip to the page, automatically handles invalid page number
+    .skip((req.query.page-1)*config.limits.userSearchPageLimit)
     .exec(function (err, users) {
       if (err) return next(err);
       return res.send(users);
